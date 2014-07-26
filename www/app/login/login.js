3e72cbea995fac01ce4580173ab5f2952784fd57
Mehul Patel <mehul8@gmail.com>
2014-07-25 21:05:59 -0700

    set up login state and ctrl

--- www/app/login/login.js
+++ www/app/login/login.js
@@ -1,1 +1,14 @@
-login.js
+angular.module('fixed.login', [])
+
+.config(function($stateProvider) {
+  $stateProvider
+  .state('app.login', {
+    url: "/login",
+    templateUrl: 'app/login/login.tpl.html',
+    controller: 'LoginCtrl'
+  });
+})
+
+.controller('LoginCtrl', function($scope, $state) {
+  console.log('LoginCtrl');
+});
