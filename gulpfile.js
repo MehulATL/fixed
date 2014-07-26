9ecd2b8d6ecb04c3d3fe465de616715618b9dd30
Mehul Patel <mehul8@gmail.com>
2014-07-25 20:43:46 -0700

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,24 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var bower = require('bower');
-var concat = require('gulp-concat');
-var sass = require('gulp-sass');
-var minifyCss = require('gulp-minify-css');
-var rename = require('gulp-rename');
-var sh = require('shelljs');
+var gulp    = require('gulp'),
+    stylus  = require('gulp-stylus');
+    notify  = require('gulp-notify');
+    concat  = require('gulp-concat');
 
 var paths = {
-  sass: ['./scss/**/*.scss']
+  stylus: 'www/stylus/**.styl',
+  css: 'www/css'
 };
 
-gulp.task('default', ['sass']);
-
-gulp.task('sass', function(done) {
-  gulp.src('./scss/ionic.app.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./www/css/'))
-    .pipe(minifyCss({
-      keepSpecialComments: 0
-    }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
-    .on('end', done);
+gulp.task('stylus', function(){
+  gulp.src(paths.stylus)
+    .pipe(stylus())
+    .pipe(gulp.dest(paths.css))
+    .pipe(notify({message: 'Stylus Compiled!'}));
 });
 
-gulp.task('watch', function() {
-  gulp.watch(paths.sass, ['sass']);
+gulp.task('build', ['stylus']);
+
+gulp.task('watch', function(){
+  gulp.watch(paths.stylus, ['build']);
 });
 
-gulp.task('install', ['git-check'], function() {
-  return bower.commands.install()
-    .on('log', function(data) {
-      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
-    });
-});
-
-gulp.task('git-check', function(done) {
-  if (!sh.which('git')) {
-    console.log(
-      '  ' + gutil.colors.red('Git is not installed.'),
-      '\n  Git, the version control system, is required to download Ionic.',
-      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
-      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
-    );
-    process.exit(1);
-  }
-  done();
-});
+gulp.task('default', ['build' ,'watch']);
