313f57b967da70a0708b334756acaba980ce3239
Mehul Patel <mehul8@gmail.com>
2014-07-25 21:07:19 -0700

    set up main state and ctrl

--- www/app/main/main.js
+++ www/app/main/main.js
@@ -1,1 +1,15 @@
-main.js
+angular.module('fixed.main', [])
+
+.config(function($stateProvider) {
+  $stateProvider
+  .state('app.main', {
+    abstract: true,
+    url: '/main',
+    templateUrl: 'app/main/main.tpl.html',
+    controller: 'MainCtrl'
+  });
+})
+
+.controller('MainCtrl', function() {
+  console.log('MainCtrl');
+});
